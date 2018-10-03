7f66cad2ff28c99e0f0b451f03c6219d590f31de
kivanova20031105 <43601190+kivanova20031105@users.noreply.github.com>
2018-10-03 11:34:51 +0300

    kachvam file dialog
    dobavqm dialog

---
+++ dialog.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    cout<<"Kak se kazvash?";
+    char ime [15];
+    cin>>ime;
+    cout<<"Zdravei"<<ime<<"!"<<endl;
+    cout<<"Kak si?";
+    char otgovor [15];
+    cin>>otgovor;
+    cout<<"okey"<<endl;
+    return 0;
+}
+
+
+
+
+
+
